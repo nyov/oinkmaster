176e9348fad63dc7b86b10c4ce1fa9c74168c639
Andreas Östling <andreas_o@users.sourceforge.net>
2002-07-13 09:35:55 +0000

    - Allow possible whitespaces in rule regexp. - Rules directory must be specified on command line. - Die with message if there were no .rules files in the rules directory.

--- contrib/makesidex.pl
+++ contrib/makesidex.pl
@@ -5,11 +5,12 @@
 # makesidex.pl - (make sid exclusions)-  Make snort SID exclusion list for 'oinkmaster.pl'
 # redirect (append!) output to your oinkmaster.conf
 
-# change this to where your snort rules are...
-my $confdir = "/temp/oinkmaster/out/";
+my $confdir = shift || die "Usage: $0 <rules directory>\n";
 
 my @files = glob "$confdir/*.rules";
 my %sid;
+
+die "No .rules files found in $confdir\n" if ($#files < 0);
 
 foreach my $file (@files) {
 	chomp $file;
@@ -18,7 +19,7 @@ foreach my $file (@files) {
 	while (my $line = <RULE>) {
 		chomp $line;
 		# look for anything that's commented out....grab sid
-		if ($line =~ /^#.*?sid\:(\d+)/) {
+		if ($line =~ /^\s*#.*?sid\s*\:\s*(\d+)/) {
 			# this is just for sorting....
 			my $key = sprintf("%06g", $1);
 			$sid{$key} = $1;
