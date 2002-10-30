3ece93d7228d1bc93473a933561dfc6fa3e44b0a
Andreas Östling <andreas_o@users.sourceforge.net>
2002-10-30 14:39:49 +0000

    less horrible

--- contrib/addmsg.pl
+++ contrib/addmsg.pl
@@ -5,13 +5,13 @@
 use strict;
 
 
-my (@config, $sid, $msg, @rulesfiles, $file, %sidmsgmap);
+my (%sidmsgmap);
 
 my $usage = "Usage: $0 <oinkmaster config file> <rules directory>\n\n".
             "New config file will be printed to standard output so you\n".
             "probably want to redirect the output to a file, for example:\n".
             "$0 oinkmaster.conf rules/ > oinkmaster.conf.new\n".
-            "If oinkmaster.conf.new looks ok, simply rename it to oinkmaster.conf.\n";
+            "If oinkmaster.conf.new looks ok, simply rename it to oinkmaster.conf.\n\n";
 
 my $snort_rule_regexp = '^\s*#*\s*(?:alert|log|pass) .+msg\s*:\s*"(.+?)"\s*;.+sid\s*:\s*(\d+)\s*;';
 
@@ -19,19 +19,18 @@ my $config   = shift || die($usage);
 my $rulesdir = shift || die($usage);
 
 
-
 # Read in oinkmaster.conf.
 open(CONFIG, "<$config") or die("could not open $config for reading: $!\n");
-@config = <CONFIG>;
+my @config = <CONFIG>;
 close(CONFIG);
 
 
 # Read in *.rules and create %sidmsgmap ($sidmsgmap{sid} = msg).
 $rulesdir =~ s/\/+$//;
-@rulesfiles = glob("$rulesdir/*.rules");
+my @rulesfiles = glob("$rulesdir/*.rules");
 die("No .rules files in $rulesdir\n") if ($#rulesfiles < 0);
 
-foreach $file (@rulesfiles) {
+foreach my $file (@rulesfiles) {
     open(RULESFILE, "<$file") or die("could not open $file: $!");
     while (<RULESFILE>) {
 	$sidmsgmap{$2} = $1 if (/$snort_rule_regexp/);
@@ -42,7 +41,7 @@ foreach $file (@rulesfiles) {
 # Print new oinkmaster.conf.
 while ($_ = shift(@config)) {
     if (/^\s*disablesids*\s+(\d+)\s*$/) {
-	$sid = $1;
+	my $sid = $1;
 	chomp;
 	s/ +/ /g;	
 	tr/\t/ /;
