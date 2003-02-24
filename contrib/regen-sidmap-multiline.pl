d1f50803dd8e6055514a23cd90c93384aeec29aa
Andreas Östling <andreas_o@users.sourceforge.net>
2003-02-24 11:53:17 +0000

    use get_next_entry(), etc

--- contrib/regen-sidmap-multiline.pl
+++ contrib/regen-sidmap-multiline.pl
@@ -2,50 +2,32 @@
 
 # $Id$ #
 
-my $usage = "usage: $0 <rulesdir>\n";
+use strict;
 
-my $rulesdir = shift || die("$usage");
+sub get_next_entry($ $ $ $);
 
+my $SNORT_RULE_REGEXP = '^\s*#*\s*(?:alert|log|pass) .+msg\s*:\s*"(.+?)"\s*;.+sid\s*:\s*(\d+)\s*;.*\)\s*$';
+my $USAGE = "usage: $0 <rulesdir>\n";
+my $rulesdir = shift || die("$USAGE");
 my %sidmap;
+
 
 opendir(RULESDIR, "$rulesdir") or die("could not open $rulesdir: $!\n");
 
-while ($file = readdir(RULESDIR)) {
+while (my $file = readdir(RULESDIR)) {
     next unless ($file =~ /\.rules$/);
 
-    open(FILE, "$rulesdir/$file") or die("could not open $rulesdir/$file: $!\n");
+    open(OLDFILE, "$rulesdir/$file") or die("could not open $rulesdir/$file: $!\n");
     print STDERR "Processing $file\n";
+    my @file = <OLDFILE>;
+    close(OLDFILE);
 
-    my ($multi, $single, $nonrule, $newfile);
+    my ($single, $multi, $nonrule);
 
-    while ($_ = <FILE>) {
-        undef($multi);
-        undef($single);
- 	undef($nonrule);
+    while (get_next_entry(\@file, \$single, \$multi, \$nonrule)) {
 
-      # Start of multi-line rule?
-        if (/^\s*(?:alert|log|pass) .*\\\s*\n$/) {
-            $multi  = $_;
-            $single = $_;
-
-          # Keep on reading as long as line ends with "\".
-            while (/\\\n$/) {
-                $single =~ s/\\\n//;
-                $_ = <FILE>;
-		$single .= $_;
-                $multi  .= $_;
-	    }
-
-      # Single-line rule?
-	} elsif (/^\s*(?:alert|log|pass)/) {
-	    $single = $_;
-        }
-
-      # Even if it was a single-line, put it in $multi now anyway.
-        $multi = $single unless (defined($multi));
-
-      # If we've got a valid rule...
-	if (defined($single) && $single =~ /msg\s*:\s*"(.+?)"\s*;.*sid\s*:\s*(\d+)\s*;/) {
+       # If we've got a valid rule...
+        if (defined($single) && $single =~ /msg\s*:\s*"(.+?)"\s*;.*sid\s*:\s*(\d+)\s*;/) {
             my $msg = $1;
             my $sid = $2;
             $sidmap{$sid} = "$sid || $msg";
@@ -58,16 +40,94 @@ while ($file = readdir(RULESDIR)) {
 
             $sidmap{$sid} .= "\n";
 
-        } elsif (defined($single)) {
-            print STDERR "Warning: unable to parse rule: $single";
+	} elsif (defined($single)) {
+	    print STDERR "Warning: unable to parse rule (missing SID?): $single";
+	} elsif ($nonrule =~ /^\s*#*alert /) {
+	    print STDERR "Warning: unable to parse rule: $nonrule";
         }
     }
-    close(FILE);
-
 }
-closedir(RULESDIR);
 
 # Print results.
 foreach my $sid (sort { $a <=> $b } keys(%sidmap)) {
     print "$sidmap{$sid}";
+}
+
+
+
+sub
+get_next_entry($ $ $ $)
+{
+    my $arr_ref     = shift;
+    my $single_ref  = shift;
+    my $multi_ref   = shift;
+    my $nonrule_ref = shift;
+
+    undef($$single_ref);
+    undef($$multi_ref);
+    undef($$nonrule_ref);
+
+    my $line = shift(@$arr_ref) || return(0);
+
+    if ($line =~ /^\s*#*\s*(?:alert|log|pass) .*\\\s*\n$/) {    # start multi-line rule?
+        $$single_ref = $line;
+        $$multi_ref  = $line;
+
+      # Keep on reading as long as line ends with "\".
+        while ($line =~ /\\\s*\n$/) {
+            $$single_ref =~ s/\s*\\\s*\n//;    # remove "\" for single-line version
+
+          # If there are no more lines, this can not be a valid multi-line rule.
+            if (!($line = shift(@$arr_ref)) || $line =~ /^\s*#/) {
+
+                $$multi_ref .= $line if (defined($line));
+
+                @_ = split(/\n/, $$multi_ref);
+
+                undef($$multi_ref);
+                undef($$single_ref);
+
+              # First line of broken multi-line rule will be returned as a non-rule line.
+                $$nonrule_ref = shift(@_) . "\n";
+
+              # The rest is put back to the array again.
+                foreach $_ (reverse((@_))) {
+                    unshift(@$arr_ref, "$_\n");
+                }
+
+                return (1);
+            }
+
+            $$single_ref .= $line;
+            $$multi_ref  .= $line;
+        }
+
+      # Single-line version should now be a valid rule.
+      # If not, it wasn't a valid multi-line rule after all.
+        if ($$single_ref =~ /$SNORT_RULE_REGEXP/) {
+            return (1);
+        } else {
+            @_ = split(/\n/, $$multi_ref);
+
+            undef($$multi_ref);
+            undef($$single_ref);
+
+          # First line of broken multi-line rule will be returned as a non-rule line.
+            $$nonrule_ref = shift(@_) . "\n";
+
+          # The rest is put back to the array again.
+            foreach $_ (reverse((@_))) {
+                unshift(@$arr_ref, "$_\n");
+            }
+
+            return (1);
+        }
+
+    } elsif ($line =~ /$SNORT_RULE_REGEXP/) {                   # single-line rule?
+        $$single_ref = $line;
+        return (1);
+    } else {                                                    # non-rule line?
+        $$nonrule_ref = $line;
+        return (1);
+    }
 }
