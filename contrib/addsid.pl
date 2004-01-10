a27e22d8240b51b9bfca81136c44f11433e5f440
Andreas Östling <andreas_o@users.sourceforge.net>
2004-01-10 15:26:34 +0000

    sync get_next_entry() and parse_singleline_rule() here as well, plus some other minor corrections

--- contrib/addsid.pl
+++ contrib/addsid.pl
@@ -5,8 +5,9 @@
 use strict;
 
 
-sub get_next_entry($ $ $ $);
-sub get_highest_sid(@);
+sub get_next_entry($ $ $ $ $ $);
+sub parse_singleline_rule($ $ $);
+sub get_next_available_sid(@);
 
 
 # Set this to the default classtype you want to add, if missing.
@@ -22,37 +23,39 @@ my $ADD_REV = 1;
 # personal use.
 my $MIN_SID = 1000001;
 
-# Regexp to match a snort rule line. SID must not be required in this one.
-my $SINGLELINE_RULE_REGEXP = '^\s*#*\s*(?:alert|drop|log|pass|reject|sdrop|activate|dynamic) '.
-                             '.+msg\s*:\s*"(.+?)"\s*;.*\)\s*$'; # ';
+# Regexp to match the start of a multi-line rule.
+# %ACTIONS% will be replaced with content of $config{actions} later.
+my $MULTILINE_RULE_REGEXP  = '^\s*#*\s*(?:%ACTIONS%)'.
+                             '\s.*\\\\\s*\n$'; # ';
 
-# Regexp to match the start (the first line) of a possible multi-line rule.
-my $MULTILINE_RULE_REGEXP = '^\s*#*\s*(?:alert|drop|log|pass|reject|sdrop|activate|dynamic)\s.*\\\\\s*\n$'; # ';
-
+# Regexp to match a single-line rule.
+my $SINGLELINE_RULE_REGEXP = '^\s*#*\s*(?:%ACTIONS%)'.
+                             '\s.+;\s*\)\s*$'; # ';
 
 my $USAGE = "usage: $0 <rulesdir> [rulesdir2, ...]\n";
-
 
 # Start in verbose mode.
 my $verbose = 1;
 
-
-my %allsids;
+my (%allsids, %config);
 
 my @rulesdirs = @ARGV;
 
 die($USAGE) unless ($#rulesdirs > -1);
 
+$config{rule_actions} = "alert|drop|log|pass|reject|sdrop|activate|dynamic";
+
+$SINGLELINE_RULE_REGEXP =~ s/%ACTIONS%/$config{rule_actions}/;
+$MULTILINE_RULE_REGEXP  =~ s/%ACTIONS%/$config{rule_actions}/;
+
 
 # Find out the next available SID.
-my $sid = get_highest_sid(@rulesdirs);
-
-# If it's below MIN_SID, use MIN_SID instead.
-$sid = $MIN_SID if ($sid < $MIN_SID);
+my $next_sid = get_next_available_sid(@rulesdirs);
 
 # Avoid seeing possible warnings about broken rules twice.
 $verbose = 0;
 
+# Add sid to active rules that don't have any.
 foreach my $dir (@rulesdirs) {
     opendir(RULESDIR, "$dir") or die("could not open $dir: $!\n");
 
@@ -65,24 +68,20 @@ foreach my $dir (@rulesdirs) {
         my @file = <OLDFILE>;
         close(OLDFILE);
 
-        open(NEWFILE, ">$dir/$file")
+        open(NEWFILE, ">", "$dir/$file")
           or die("could not open $dir/$file for writing: $!\n");
 
-        my ($single, $multi, $nonrule);
-        while (get_next_entry(\@file, \$single, \$multi, \$nonrule)) {
+        my ($single, $multi, $nonrule, $msg, $sid);
+        while (get_next_entry(\@file, \$single, \$multi, \$nonrule, \$msg, \$sid)) {
 
             if (defined($nonrule)) {
    	        print NEWFILE "$nonrule";
 	        next;
             }
 
-          # Grab msg.
-            $single =~ /$SINGLELINE_RULE_REGEXP/oi;
-            my $msg = $1;
-
             $multi = $single unless (defined($multi));
 
-          # Don't care about inactive rules.
+          # Don't care about inactive rules when adding sids.
             if ($single =~ /^\s*#/) {
 	        print NEWFILE "$multi";
 	        next;
@@ -90,9 +89,9 @@ foreach my $dir (@rulesdirs) {
 
           # Add SID.
             if ($single !~ /sid\s*:\s*\d+\s*;/) {
-                print "Adding SID $sid to rule \"$msg\"\n";
-                $multi =~ s/\)\s*\n/sid:$sid;)\n/;
-                $sid++;
+                print "Adding SID $next_sid to rule \"$msg\"\n";
+                $multi =~ s/\)\s*\n/sid:$next_sid;)\n/;
+                $next_sid++;
             }
 
           # Add revision.
@@ -115,8 +114,9 @@ foreach my $dir (@rulesdirs) {
 }
 
 
+
 # Read in *.rules in given directory and return highest SID.
-sub get_highest_sid(@)
+sub get_next_available_sid(@)
 {
     my @dirs = @_;
 
@@ -131,16 +131,13 @@ sub get_highest_sid(@)
             my @file = <OLDFILE>;
             close(OLDFILE);
 
-            my ($single, $multi, $nonrule);
+            my ($single, $multi, $nonrule, $msg, $sid);
 
-            while (get_next_entry(\@file, \$single, \$multi, \$nonrule)) {
-                if (defined($single) && $single =~ /sid\s*:(\d+)\s*;/) {
-	            my $tmpsid = $1;
-
-                    print STDERR "WARNING: duplicate SID: $tmpsid\n"
-	              if (exists($allsids{$tmpsid}));
-
-   	            $allsids{$tmpsid}++;
+            while (get_next_entry(\@file, \$single, \$multi, \$nonrule, \$msg, \$sid)) {
+                if (defined($single) && defined($sid)) {
+                    print STDERR "WARNING: duplicate SID: $sid\n"
+	              if (exists($allsids{$sid}));
+   	            $allsids{$sid}++;
                 }
             }
         }
@@ -151,42 +148,56 @@ sub get_highest_sid(@)
     my $sid = pop(@_);
 
     if (!defined($sid)) {
-        $sid = $MIN_SID 
+        $sid = $MIN_SID
     } else {
         $sid++;
     }
+
+  # If it's below MIN_SID, use MIN_SID instead.
+    $sid = $MIN_SID if ($sid < $MIN_SID);
 
     return ($sid)
 }
 
 
 
-sub get_next_entry($ $ $ $)
+sub get_next_entry($ $ $ $ $ $)
 {
     my $arr_ref     = shift;
     my $single_ref  = shift;
     my $multi_ref   = shift;
     my $nonrule_ref = shift;
+    my $msg_ref     = shift;
+    my $sid_ref     = shift;
 
     undef($$single_ref);
     undef($$multi_ref);
     undef($$nonrule_ref);
+    undef($$msg_ref);
+    undef($$sid_ref);
 
     my $line = shift(@$arr_ref) || return(0);
+    my $disabled = 0;
+    my $broken   = 0;
 
-    if ($line =~ /$MULTILINE_RULE_REGEXP/oi) {    # possible beginning of multi-line rule?
+  # Possible beginning of multi-line rule?
+    if ($line =~ /$MULTILINE_RULE_REGEXP/oi) {
         $$single_ref = $line;
         $$multi_ref  = $line;
 
+        $disabled = 1 if ($line =~ /\s*#/);
+
       # Keep on reading as long as line ends with "\".
-        while ($line =~ /\\\s*\n$/) {
-            $$single_ref =~ s/\\\s*\n//;    # remove trailing "\" for single-line version
+        while (!$broken && $line =~ /\\\s*\n$/) {
+
+          # Remove trailing "\" and newline for single-line version.
+            $$single_ref =~ s/\\\s*\n//;
 
           # If there are no more lines, this can not be a valid multi-line rule.
             if (!($line = shift(@$arr_ref))) {
 
-                warn("WARNING: got EOF while parsing multi-line rule: $$multi_ref\n")
-                  if ($verbose);
+                warn("\nWARNING: got EOF while parsing multi-line rule: $$multi_ref\n")
+                  if ($config{verbose});
 
                 @_ = split(/\n/, $$multi_ref);
 
@@ -195,7 +206,7 @@ sub get_next_entry($ $ $ $)
 
               # First line of broken multi-line rule will be returned as a non-rule line.
                 $$nonrule_ref = shift(@_) . "\n";
-                $$nonrule_ref =~ s/\s*\n$/\n/;            # remove trailing whitespaces
+                $$nonrule_ref =~ s/\s*\n$/\n/;    # remove trailing whitespaces
 
               # The rest is put back to the array again.
                 foreach $_ (reverse((@_))) {
@@ -207,18 +218,27 @@ sub get_next_entry($ $ $ $)
 
           # Multi-line continuation.
             $$multi_ref .= $line;
-            $line =~ s/^\s*#*\s*//;     # In single-line version, remove leading #'s first
-            $$single_ref .= $line;
+
+          # If there are non-comment lines in the middle of a disabled rule,
+          # mark the rule as broken to return as non-rule lines.
+            if ($line !~ /\s*#/ && $disabled) {
+                $broken = 1;
+            } elsif ($line =~ /\s*#/ && !$disabled) {
+                # comment line (with trailing slash) in the middle of an active rule - ignore it
+            } else {
+                $line =~ s/^\s*#*\s*//;  # remove leading # in single-line version
+                $$single_ref .= $line;
+            }
 
         } # while line ends with "\"
 
       # Single-line version should now be a valid rule.
       # If not, it wasn't a valid multi-line rule after all.
-        if ($$single_ref =~ /$SINGLELINE_RULE_REGEXP/oi) {
+        if (!$broken && parse_singleline_rule($$single_ref, $msg_ref, $sid_ref)) {
 
-            $$single_ref =~ s/^\s*//;        # remove leading whitespaces
-            $$single_ref =~ s/^#+\s*/#/;     # remove whitespaces next to the leading #
-            $$single_ref =~ s/\s*\n$/\n/;    # remove trailing whitespaces
+            $$single_ref =~ s/^\s*//;     # remove leading whitespaces
+            $$single_ref =~ s/^#+\s*/#/;  # remove whitespaces next to leading #
+            $$single_ref =~ s/\s*\n$/\n/; # remove trailing whitespaces
 
             $$multi_ref  =~ s/^\s*//;
             $$multi_ref  =~ s/\s*\n$/\n/;
@@ -226,8 +246,8 @@ sub get_next_entry($ $ $ $)
 
             return (1);   # return multi
         } else {
-            warn("WARNING: invalid multi-line rule: $$single_ref\n")
-              if ($verbose && $$multi_ref !~ /^\s*#/);
+            warn("\nWARNING: invalid multi-line rule: $$single_ref\n")
+              if ($config{verbose} && $$multi_ref !~ /^\s*#/);
 
             @_ = split(/\n/, $$multi_ref);
 
@@ -245,23 +265,54 @@ sub get_next_entry($ $ $ $)
 
             return (1);   # return non-rule
         }
-
-    } elsif ($line =~ /$SINGLELINE_RULE_REGEXP/oi) {  # regular single-line rule?
+     } elsif (parse_singleline_rule($line, $msg_ref, $sid_ref)) {
         $$single_ref = $line;
-        $$single_ref =~ s/^\s*//;            # remove leading whitespaces
-        $$single_ref =~ s/^#+\s*/#/;         # remove whitespaces next to the leading #
-        $$single_ref =~ s/\s*\n$/\n/;        # remove trailing whitespaces
+        $$single_ref =~ s/^\s*//;
+        $$single_ref =~ s/^#+\s*/#/;
+        $$single_ref =~ s/\s*\n$/\n/;
 
         return (1);   # return single
-    } else {                                 # non-rule line?
+    } else {                          # non-rule line
 
-      # Do extra check and warn if it *might* be a rule anyway, but that we couldn't parse.
-        warn("WARNING: line may be a rule but it could not be parsed: $line\n")
-          if ($verbose && $line =~ /^\s*alert .+msg\s*:\s*".+"\s*;/);
+      # Do extra check and warn if it *might* be a rule anyway,
+      # but that we just couldn't parse for some reason.
+        warn("\nWARNING: line may be a rule but it could not be parsed ".
+             "(missing sid or msg?): $line\n")
+          if ($config{verbose} && $line =~ /^\s*alert .+msg\s*:\s*".+"\s*;/);
 
         $$nonrule_ref = $line;
-        $$nonrule_ref =~ s/\s*\n$/\n/;       # remove trailing whitespaces
+        $$nonrule_ref =~ s/\s*\n$/\n/;
 
         return (1);   # return non-rule
     }
+}
+
+
+
+# From oinkmaster.pl except that this version
+# has been modified so that the sid is *optional*.
+sub parse_singleline_rule($ $ $)
+{
+    my $line    = shift;
+    my $msg_ref = shift;
+    my $sid_ref = shift;
+
+    if ($line =~ /$SINGLELINE_RULE_REGEXP/oi) {
+
+        if ($line =~ /msg\s*:\s*"(.+?)"\s*;/i) {
+            $$msg_ref = $1;
+        } else {
+            return (0);
+        }
+
+        if ($line =~ /sid\s*:\s*(\d+)\s*;/i) {
+            $$sid_ref = $1;
+#        } else {
+#            return (0);
+        }
+
+        return (1);
+    }
+
+    return (0);
 }
